e7949ffcd3ba1820c0c6ebe8d590d185b30bbbb4
Ureeces <unconfigured@null.spigotmc.org>
2020-10-13 15:02:19 -0400

    Added more games to games.js

--- models/games.js
+++ models/games.js
@@ -1,9 +1,25 @@
 const games = [
     {
-        id: 1,
+        id: '1',
         name: 'Halo: Combat Evolved',
         description: 'Super-soldier John-117, Master Chief of the United Nations Space Command, must battle a genocidal alien race known as the Covenant following his violent crash-landing on Halo, an ancient and mysterious ring-world.',
-        yearReleased: 2001,
+        yearReleased: '2001',
         playTime: '10 hours'
+    },
+
+    {
+        id: '2',
+        name: 'UNDERTALE',
+        description: `In this RPG, you don't have to kill anyone. Each enemy can be "defeated" nonviolently. Dance with a slime. Pet a dog. Whisper your favorite secret to a knight. Or, ignore this choice and rain destruction upon your foes.`,
+        yearReleased: '2015',
+        playTime: '6 hours'
+    },
+
+    {
+        id: '3',
+        name: 'Phasmophobia',
+        description: `Paranormal activity is on the rise and it’s up to you and your team to use all the ghost hunting equipment at your disposal in order to gather as much evidence as you can.`,
+        yearReleased: '2020',
+        playTime: '30+ mins per round'
     }
 ];
