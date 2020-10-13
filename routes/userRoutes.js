482ba556bd9cd956d56be79feb0d387eb7d72222
Ureeces <unconfigured@null.spigotmc.org>
2020-10-13 15:39:33 -0400

    Completed getSingleGame (id version.)

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,4 +7,17 @@ router.get('/getAllGames', (req, res) => {
     return res.status(200).json({ confirmation: 'success', games });
 });
 
+// getSingleGame
+router.get('/getSingleGame/:id', (req, res) => {
+    let foundGame = games.filter((game) => {
+        if(game.id === req.params.id) {
+            return res.status(200).json({ confirmation: 'success', game });
+        }
+    });
+
+        if(!foundGame.length) {
+            return res.status(400).json({ confirmation: 'fail', message: `Game not found.` })
+        }
+})
+
 module.exports = router;
