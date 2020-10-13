8b81253297a476a1052ecd93b2f6101c1714c636
Ureeces <unconfigured@null.spigotmc.org>
2020-10-13 15:05:59 -0400

    Completed app.js skeleton.

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ require('dotenv').config();
 const userRoutes = require('./routes/userRoutes');
 
 const port = process.env.PORT || 3000;
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('api/v1/games', userRoutes);
+
+// opening the ports
+app.listen(port, () => {
+    console.log('Listening on:', port);
+})
